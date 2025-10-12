a47dc2197df7c70efb8c033fe21a86a912609c3c
nico mueller <hans@hans.de>
2025-10-12 21:01:01 +0200

    schema.ts Line 109 Table: Unit, id auf   id: integer("id").primaryKey({autoIncrement: true}),

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -106,7 +106,7 @@ export const ingredientCat = sqliteTable("INGREDIENT_CAT", {
 });
 
 export const unit = sqliteTable("UNIT", {
-  id: integer("id").primaryKey(),
+  id: integer("id").primaryKey({autoIncrement: true}),
   name: text("name").notNull(),
   shortForm: text("short_form"),
 });
