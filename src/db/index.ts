6735afc5c2aa39ec551b84cca40a61c556df3957
Melina Klein <mel.klein.2x4@lehre.mosbach.dhbw.de>
2025-10-14 16:22:52 +0200

    change index back

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
-const client = createClient({
-  url: "file:./src/db/localdb.sqlite", // This is undefined
+export const db = drizzle({
+  connection: {
+    // biome-ignore lint/style/noNonNullAssertion: We have a .env file so we expect this to be fine
+    url: process.env.DATABASE_URL!,
+  },
 });
-
-export const db = drizzle(client);
