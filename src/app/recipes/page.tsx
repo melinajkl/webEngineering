fa169ea88dfba475b2f718a5c3856518ca4b646b
Melina Klein <mel.klein.2x4@lehre.mosbach.dhbw.de>
2025-10-15 22:36:08 +0200

    add create recipe button

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,15 +1,23 @@
 import { NavigationBar } from "@/components/NavigationBar";
 import RecipeOverview from "@/components/RecipeOverview";
+import OpenCreateRecipeButton from "./_components/open_create_recipe_button";
 
-export default function Page() {
+interface PageProps {
+  searchParams: Promise<{ page?: string }>;
+}
+
+export default function Page({ searchParams }: PageProps) {
   return (
     <div className="flex flex-col min-h-screen">
       <NavigationBar />
-      <div className="flex-1 overflow-auto">
-        <RecipeOverview searchParams={searchParams} />
+      <div className="bg-gray-50 p-8">
+        <div className="container grid-cols-3 overflow-auto p-3 max-w-7xl mx-auto">
+          <OpenCreateRecipeButton label="Rezept erstellen" />
+        </div>
+        <div className="flex-1 overflow-auto">
+          <RecipeOverview searchParams={searchParams} />
+        </div>
       </div>
     </div>
   );
 }
-
-
