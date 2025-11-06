a6a9afd02c92ec8ef70df7a24eafa4031e6957e1
Melina Klein <mel.klein.2x4@lehre.mosbach.dhbw.de>
2025-11-06 13:43:28 +0100

    delete date of use property in database

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -82,7 +82,6 @@ export const recipeSteps = sqliteTable("RECIPE_STEPS", {
 export const shoppingList = sqliteTable("SHOPPING_LIST", {
   id: integer("id").primaryKey({autoIncrement : true}),
   ingredientId: integer("ingredient_id").references(() => ingredients.id).notNull(),
-  dateOfUse: real("date_of_use").notNull(),
   amount: integer("amount").notNull(),
   unitId: integer("unitId").references(() => unit.id).notNull(),
   checked: integer("checked", { mode: "boolean" }).notNull(),
